9db5a13d6c71f7b73143384e888bf6c75a8ce994
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2022-03-21 19:33:58 +0900

    feat: テストコード

---
+++ infrastructures/repositories/oauth_access_token_repository_test.go
@@ -0,0 +1,35 @@
+package repositories
+
+import (
+	"context"
+	"github.com/golang/mock/gomock"
+	"github.com/hiroyky/famiphoto/infrastructures/models"
+	mock_repositories "github.com/hiroyky/famiphoto/testing/mocks/infrastructures/repositories"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+func TestOauthAccessTokenRepository_SetClientCredentialAccessToken(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	clientID := "client_id1"
+	accessToken := "access_token"
+	expireIn := int64(1000)
+	content, _ := (&models.OauthAccessToken{
+		ClientID:   clientID,
+		ClientType: models.OauthClientTypeClientCredential,
+	}).String()
+
+	redisAdapter := mock_repositories.NewMockRedisAdapter(ctrl)
+	redisAdapter.EXPECT().SetEx(gomock.Any(), gomock.Any(), content, time.Duration(expireIn)*time.Second)
+
+	repo := oauthAccessTokenRepository{
+		db:     redisAdapter,
+		prefix: "prefix_",
+	}
+
+	err := repo.SetClientCredentialAccessToken(context.Background(), clientID, accessToken, expireIn)
+	assert.NoError(t, err)
+}
