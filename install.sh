1583bd8cbf086524cfd2b1b12710348c740bc4ff
Hirokazu Yokoyama <hiroyky.voyager@gmail.com>
2022-12-10 18:33:16 +0900

    fix

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ mkdir -p ./dst
 WEB_ENV=./dst/.env.web.prod
 API_ENV=./dst/.env.prod
 
-echo "API_BASE_URL=http://famiphoto:8080" >> ${WEB_ENV}
+echo "API_BASE_URL=http://famiphoto:8080" > ${WEB_ENV}
 echo "IS_DEBUG=0" >> ${WEB_ENV}
 
 CLIENT_SECRET=`cat /dev/urandom |LC_CTYPE=C tr -dc '[:alnum:]' | fold -w 50 |head -n 1`
@@ -16,7 +16,7 @@ echo "CLIENT_ID=famiphoto_web" >> ${WEB_ENV}
 echo "CLIENT_SECRET=${CLIENT_SECRET}" >> ${WEB_ENV}
 echo "SESSION_SECRET=${SESSION_SECRET}" >> ${WEB_ENV}
 
-echo "WEB_CLIENT_ID=famiphoto_web" >> ${API_ENV}
+echo "WEB_CLIENT_ID=famiphoto_web" > ${API_ENV}
 echo "WEB_CLIENT_SECRET=${CLIENT_SECRET}" >> ${API_ENV}
 
 
